53aef2f8c5766602d4c0304ed08f6299e46ca6d9
Megan <meganelizabethgoode@hotmail.com>
2020-08-23 22:12:50 +0100

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">Navbar</span>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
